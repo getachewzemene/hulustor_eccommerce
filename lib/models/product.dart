6aefb41828a6d3518ff890ea75e054dbd364a644
getachewzemene <getchze1221@gmail.com>
2022-04-30 08:30:21 +0300

    initial commit

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  int id;
+  String name;
+  double price;
+  String detailSummary;
+  String imageURL;
+  Product(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.detailSummary,
+      required this.imageURL});
+}
---
+++ lib/widgets/nav_drawer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
